c544ad49fe86330094f7d75fc266e83875ef7819
Joseph Lee <joseph@jc-lab.net>
2022-09-12 17:42:12 +0900

    fix: export on browser

--- src/browser.ts
+++ src/browser.ts
@@ -1,8 +1,15 @@
 import { ConnectMessage } from './ws-model';
 
+export * from './types';
 export * from './client-base';
 export * from './server';
 export * from './client';
+
+import { globalInit } from './utils';
+
+export {
+  globalInit
+};
 
 export {
   ConnectMessage
